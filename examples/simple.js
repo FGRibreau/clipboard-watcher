8d5ae4792605b8089b1d515691e98e17a9ee35c5
FG Ribreau <github@fgribreau.com>
2014-08-10 15:56:45 +0200

    First commit

---
+++ examples/simple.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var PbWatcher = require('../');
+var watchIntervalMs = 250;
+
+PbWatcher(function(data){
+  console.log('Clipboard changed to : "%s"', data);
+}, watchIntervalMs);
---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var $    = require('NodObjC');
+$.framework('AppKit');
+var pool = $.NSAutoreleasePool('alloc')('init');
+
+/**
+ * [exports description]
+ * @param  {Function} f(type, data) listener
+ * @return {[type]}
+ */
+
+module.exports = function(f, interval){
+  var current;
+  interval = interval || module.exports.DEFAULT_INTERVAL;
+
+  function tick(){
+    var data = String(getClipboard());
+
+    if(data !== null && data !== current){
+      current = data;
+      f(current);
+    }
+
+    setTimeout(tick, interval);
+  }
+
+  tick();
+};
+
+module.exports.DEFAULT_INTERVAL = 250;
+
+/**
+ * [getClipboard description]
+ * @optional
+ * @param  {String} type
+ *
+ * @return {String} clipboard data
+ */
+function getClipboard(type){
+  var x  = $.NSPasteboard('generalPasteboard');
+  var tt = mytype(type);
+  var t  = tt[0];
+  var o  = tt[1];
+  return o === 's' ? x('stringForType', t) : null;
+}
+
+/**
+ * [mytype description]
+ * @param  {String} type
+ * @optional
+ * @return {[NS, String]}
+ */
+function mytype(type) {
+  if(!type){
+    return [$.NSStringPboardType, 's'];
+  }
+
+  type = type.toLowerCase();
+
+  if(type === 'html'){
+    return [$.NSHTMLPboardType, 's'];
+  }
+
+  if(type === 'png'){
+    return [$.NSHTMLPboardType, 'i'];
+  }
+
+  if(type === 'text'){
+    return [$.NSStringPboardType, 's'];
+  }
+
+  return [$[type], 's'];
+}
